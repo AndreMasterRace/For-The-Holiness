eb44319dac7c8da4980279432c2c10ca540e3ac8
Vítor Meira <vhcm21@hotmail.com>
2017-01-05 12:51:27 +0000

    Commit

--- For The Holiness/Assets/Scripts/Aimbot.cs
+++ For The Holiness/Assets/Scripts/Aimbot.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class Aimbot : MonoBehaviour {
 
+    private Vector3 dir;
+    public GameObject prefab;
+    private GameObject projectile;
+
 	void Start()
     {
-
     }
 
     void FixedUpdate()
     {
-
+        GameObject projectile = (GameObject)Instantiate(prefab, transform.position, Quaternion.identity);
+        projectile.transform.position = transform.position + Vector3.up;
     }
 }
